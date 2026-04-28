eb7618b0eaba985bed1841d5fa0bce44c6990a26
laurentnicolaspro-a11y <laurent.nicolaspro23@gmail.com>
2026-04-28 10:18:49 +0200

    Delete api/audio-proxy.js

--- api/audio-proxy.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
-  const { path } = req.query;
-  if (!path) return res.status(400).json({ error: 'path requis' });
-  const url = `${process.env.SUPABASE_URL}/storage/v1/object/public/audio/${path}`;
-  return res.redirect(302, url);
-};
